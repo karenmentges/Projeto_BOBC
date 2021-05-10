8af8d9714df39d9eac6d2a89f7b9b7a8a5b52db8
karenmentges <karen_ruvermentges@hotmail.com>
2021-05-10 20:28:24 -0300

    Changes

--- testbench.v
+++ testbench.v
@@ -1,8 +1,32 @@
 module testbench;
 
 parameter X = 2, A = 3, B = 4, C = 5;
+reg ck = 0, inicio = 0, pronto = 0, rst = 0;
 
-reg ck = 0, rst = 0, w = 0;
+wire lx, h, ls, lh, done;
+wire [1:0] m0, m1, m2; 
+
+input ck,
+    input rst,
+    input lx,
+    input [1:0] m0,
+    input [1:0] m1,
+    input [1:0] m2,
+    input h,
+    input ls,
+    input lh,
+    input done,
+    input [7:0] X,
+    input [15:0] A,
+    input [15:0] B,
+    input [15:0] C,
+    output [15:0] Resultado
+
+controle c();
+operativo op();
+
+///////////////////////////////////////
+
 wire done1, done2;
 
 wire [5:0] barramento1;
