6b7a82785904c28cfbd77922344d1d2aebc5d108
karenmentges <karen_ruvermentges@hotmail.com>
2021-05-10 18:53:57 -0300

    Changes

--- controle.v
+++ controle.v
@@ -13,28 +13,25 @@ module controle (
     output done
 );
 
-reg [1:0] state;
+reg [1:0] state = 0; // inicializa em A
 
 parameter A = 0, B = 1, C = 2, D = 3, E = 4, F = 5, G = 6, H = 7, I = 8, J = 9;
 
 
 always @(posedge ck or rst) begin //Clock na borda de subida
-    if(rst == 1) 
+    if(rst == 1)
+        assign lx = 1'b0; 
+        assign m0 = 2'b00;
+        assign m1 = 2'b00;
+        assign m2 = 2'b00;
+        assign h = 1'b0;
+        assign ls = 1'b0;
+        assign lh = 1'b0;
+        assign done = 1'b0;
         state <= A;
     else begin
         case (state)
             A : begin
-                assign lx = 1'b0; 
-                assign m0 = 2'b00;
-                assign m1 = 2'b00;
-                assign m2 = 2'b00;
-                assign h = 1'b0;
-                assign ls = 1'b0;
-                assign lh = 1'b0;
-                assign done = 1'b0;
-                state <= B;
-            end
-            B : begin
                 assign lx = 1'b1; 
                 assign m0 = 2'b01; 
                 assign m1 = 2'b00;
@@ -43,9 +40,9 @@ always @(posedge ck or rst) begin //Clock na borda de subida
                 assign ls = 1'b0; 
                 assign lh = 1'b0; 
                 assign done = 1'b0;
-                state <= C;
+                state <= B;
             end
-            C : begin
+            B : begin
                 assign lx = 1'b0;
                 assign m0 = 2'b01;
                 assign m1 = 2'b00;
@@ -53,6 +50,17 @@ always @(posedge ck or rst) begin //Clock na borda de subida
                 assign h = 1'b1;
                 assign ls = 1'b0;
                 assign lh = 1'b1;
+                assign done = 1'b0;
+                state <= C;
+            end
+            C : begin
+                assign lx = 1'b0;
+                assign m0 = 2'b10;
+                assign m1 = 2'b00;
+                assign m2 = 2'b11;
+                assign h = 1'b0;
+                assign ls = 1'b0;
+                assign lh = 1'b0;
                 assign done = 1'b0;
                 state <= D;
             end
@@ -63,18 +71,18 @@ always @(posedge ck or rst) begin //Clock na borda de subida
                 assign m2 = 2'b11;
                 assign h = 1'b0;
                 assign ls = 1'b0;
-                assign lh = 1'b0;
+                assign lh = 1'b1;
                 assign done = 1'b0;
                 state <= E;
             end
             E : begin
                 assign lx = 1'b0;
-                assign m0 = 2'b10;
-                assign m1 = 2'b00;
+                assign m0 = 2'b00;
+                assign m1 = 2'b01;
                 assign m2 = 2'b11;
-                assign h = 1'b0;
+                assign h = 1'b1;
                 assign ls = 1'b0;
-                assign lh = 1'b1;
+                assign lh = 1'b0;
                 assign done = 1'b0;
                 state <= F;
             end
@@ -85,18 +93,18 @@ always @(posedge ck or rst) begin //Clock na borda de subida
                 assign m2 = 2'b11;
                 assign h = 1'b1;
                 assign ls = 1'b0;
-                assign lh = 1'b0;
+                assign lh = 1'b1;
                 assign done = 1'b0;
                 state <= G;
             end
             G : begin
                 assign lx = 1'b0;
-                assign m0 = 2'b00;
-                assign m1 = 2'b01;
+                assign m0 = 2'b11;
+                assign m1 = 2'b00;
                 assign m2 = 2'b11;
-                assign h = 1'b1;
+                assign h = 1'b0;
                 assign ls = 1'b0;
-                assign lh = 1'b1;
+                assign lh = 1'b0;
                 assign done = 1'b0;
                 state <= H;
             end
@@ -106,23 +114,12 @@ always @(posedge ck or rst) begin //Clock na borda de subida
                 assign m1 = 2'b00;
                 assign m2 = 2'b11;
                 assign h = 1'b0;
-                assign ls = 1'b0;
+                assign ls = 1'b1;
                 assign lh = 1'b0;
                 assign done = 1'b0;
                 state <= I;
             end
             I : begin
-                assign lx = 1'b0;
-                assign m0 = 2'b11;
-                assign m1 = 2'b00;
-                assign m2 = 2'b11;
-                assign h = 1'b0;
-                assign ls = 1'b1;
-                assign lh = 1'b0;
-                assign done = 1'b0;
-                state <= J;
-            end
-            J : begin
                 assign lx = 1'b0;
                 assign m0 = 2'b00;
                 assign m1 = 2'b00;
@@ -131,6 +128,17 @@ always @(posedge ck or rst) begin //Clock na borda de subida
                 assign ls = 1'b0;
                 assign lh = 1'b0;
                 assign done = 1'b1;
+                state <= J;
+            end
+            J : begin
+                assign lx = 1'b0; 
+                assign m0 = 2'b00;
+                assign m1 = 2'b00;
+                assign m2 = 2'b00;
+                assign h = 1'b0;
+                assign ls = 1'b0;
+                assign lh = 1'b0;
+                assign done = 1'b0;
                 state <= A;
             end
         endcase    
