cf5fd3a12bd2aebbf17a0fa964d811ee47182f19
Izabela <izabela.fusieger@outlook.com>
2021-05-10 18:37:05 -0300

    controle

--- controle.v
+++ controle.v
@@ -4,9 +4,9 @@ module controle (
     input pronto,
     input rst,
     output lx,
-    output m0,
-    output m1,
-    output m2,
+    output[1:0] m0,
+    output[1:0] m1,
+    output[1:0] m2,
     output h,
     output ls,
     output lh,
@@ -17,30 +17,6 @@ reg [1:0] state;
 
 parameter A = 0, B = 1, C = 2, D = 3, E = 4, F = 5, G = 6, H = 7, I = 8, J = 9;
 
-always @(*) begin
-    case (state)
-    A : begin
-        lx=1'b0; m0=2'b00; m1=2'b00; m2=2'b00; h=1'b0; ls=1'b0; lh=1'b0; done=1'b0;
-    B : begin
-        lx=1'b1; m0=2'b01; m1=2'b00; m2=2'b00; h=1'b1; ls=1'b0; lh=1'b0; done=1'b0;
-    C : begin
-        lx=1'b0; m0=2'b01; m1=2'b00; m2=2'b00; h=1'b1; ls=1'b0; lh=1'b1; done=1'b0;
-    D : begin
-        lx=1'b0; m0=2'b10; m1=2'b00; m2=2'b11; h=1'b0; ls=1'b0; lh=1'b0; done=1'b0;
-    E : begin
-        lx=1'b0; m0=2'b10; m1=2'b00; m2=2'b11; h=1'b0; ls=1'b0; lh=1'b1; done=1'b0;
-    F : begin
-        lx=1'b0; m0=2'b00; m1=2'b01; m2=2'b11; h=1'b1; ls=1'b0; lh=1'b0; done=1'b0;
-    G : begin
-        lx=1'b0; m0=2'b00; m1=2'b01; m2=2'b11; h=1'b1; ls=1'b0; lh=1'b1; done=1'b0;
-    H : begin
-        lx=1'b0; m0=2'b11; m1=2'b00; m2=2'b11; h=1'b0; ls=1'b0; lh=1'b0; done=1'b0;
-    I : begin
-        lx=1'b0; m0=2'b11; m1=2'b00; m2=2'b11; h=1'b0; ls=1'b1; lh=1'b0; done=1'b0;
-    J : begin
-        lx=1'b0; m0=2'b00; m1=2'b00; m2=2'b00; h=1'b0; ls=1'b0; lh=1'b0; done=1'b1;
-    endcase
-end
 
 always @(posedge ck or rst) begin //Clock na borda de subida
     if(rst == 1) 
@@ -48,37 +24,116 @@ always @(posedge ck or rst) begin //Clock na borda de subida
     else begin
         case (state)
             A : begin
+                lx=1'b0; 
+                m0=2'b00;
+                m1=2'b00;
+                m2=2'b00;
+                h=1'b0;
+                ls=1'b0;
+                lh=1'b0;
+                done=1'b0;
                 state <= B;
             end
             B : begin
+                lx=1'b1; 
+                m0=2'b01; 
+                m1=2'b00;
+                m2=2'b00; 
+                h=1'b1; 
+                ls=1'b0; 
+                lh=1'b0; 
+                done=1'b0;
                 state <= C;
             end
             C : begin
+                lx=1'b0;
+                m0=2'b01;
+                m1=2'b00;
+                m2=2'b00;
+                h=1'b1;
+                ls=1'b0;
+                lh=1'b1;
+                done=1'b0;
                 state <= D;
             end
             D : begin
+                lx=1'b0;
+                m0=2'b10;
+                m1=2'b00;
+                m2=2'b11;
+                h=1'b0;
+                ls=1'b0;
+                lh=1'b0;
+                done=1'b0;
                 state <= E;
             end
             E : begin
+                lx=1'b0;
+                m0=2'b10;
+                m1=2'b00;
+                m2=2'b11;
+                h=1'b0;
+                ls=1'b0;
+                lh=1'b1;
+                done=1'b0;
                 state <= F;
             end
             F : begin
+                lx=1'b0;
+                m0=2'b00;
+                m1=2'b01;
+                m2=2'b11;
+                h=1'b1;
+                ls=1'b0;
+                lh=1'b0;
+                done=1'b0;
                 state <= G;
             end
             G : begin
+                lx=1'b0;
+                m0=2'b00;
+                m1=2'b01;
+                m2=2'b11;
+                h=1'b1;
+                ls=1'b0;
+                lh=1'b1;
+                done=1'b0;
                 state <= H;
             end
             H : begin
+                lx=1'b0;
+                m0=2'b11;
+                m1=2'b00;
+                m2=2'b11;
+                h=1'b0;
+                ls=1'b0;
+                lh=1'b0;
+                done=1'b0;
                 state <= I;
             end
             I : begin
+                lx=1'b0;
+                m0=2'b11;
+                m1=2'b00;
+                m2=2'b11;
+                h=1'b0;
+                ls=1'b1;
+                lh=1'b0;
+                done=1'b0;
                 state <= J;
             end
             J : begin
+                lx=1'b0;
+                m0=2'b00;
+                m1=2'b00;
+                m2=2'b00;
+                h=1'b0;
+                ls=1'b0;
+                lh=1'b0;
+                done=1'b1;
                 state <= A;
             end
         endcase    
     end
 end
-
 endmodule
